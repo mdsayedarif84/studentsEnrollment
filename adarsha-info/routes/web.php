78a965fc6d99e7345619fdca926cbc0a5d5ea595
mdsayedarif84 <mdsayedarif84@gmail.com>
2023-04-16 21:55:36 +0600

    sign in & up & admin assets link complete

--- adarsha-info/routes/web.php
+++ adarsha-info/routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('student-login');
+});
+Route::get('/backend', function () {
+    return view('admin.admin-login');
 });
